1e02609b0a001efb9cda9107925e01617393d8b0
Arturo Job Escobar Medina <javabev2002@gmail.com>
2025-12-06 22:33:19 -0600

    modifque el js

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,22 @@
 const boton = document.getElementById("btn-cambiar");
 const mensaje = document.getElementById("mensaje");
 const textoResaltadas = DocumentFragment.queryDelectorAll(".texto-Resaltado");
+
+
+boton.addEventListener("click", () => {
+    mensaje.textContent = "¡Has hecho clic en el botón!";
+
+    textoResaltadas.forEach((elemento) => {
+        package.textcontent = "Texto Modificado";
+    });
+});
+//prueba 
+// prueba 2 
+// prueba 3
+// prueba 4
+// prueba 5
+// prueba 6
+
+
+
+    
