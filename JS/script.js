202a533abe28a822c1da61046c2151fed7a2db74
Arturo Job Escobar Medina <javabev2002@gmail.com>
2025-12-06 22:04:53 -0600

    upload project

---
+++ JS/script.js
@@ -0,0 +1,3 @@
+const boton = document.getElementById("btn-cambiar");
+const mensaje = document.getElementById("mensaje");
+const textoResaltadas = DocumentFragment.queryDelectorAll(".texto-Resaltado");
